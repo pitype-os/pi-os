d07c2c067acd04f182db2ce13685c997e3beb60b
Rafael <rafael.prevost.15@gmail.com>
2025-06-09 10:47:59 -0400

    Clean up

--- cBits/idris_support.c
+++ cBits/idris_support.c
@@ -41,3 +41,46 @@ READADDR(Char)
 
 size_t idris2_heap_size() { return HEAP_SIZE; }
 char* idris2_heap_start() { return HEAP_START; }
+
+// utils
+
+char *UART=0x10000000;
+
+void putChar(char c) {
+	*UART=c;
+}
+
+void print(char *str) {
+	while(*str!='\0') {
+		putChar(*str);
+		str++;
+	}
+
+	putChar('\n');
+}
+
+char* itoa(int value, char* result, int base) {
+    // check that the base if valid
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while ( value );
+
+    // Apply negative sign
+    if (tmp_value < 0) *ptr++ = '-';
+    *ptr-- = '\0';
+  
+    // Reverse the string
+    while(ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    return result;
+}
