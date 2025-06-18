c35c5bc5096b8e80c0710501b8f01eae9af06bb3
rafaelRiv <rafael@evernow.com>
2025-06-17 22:44:55 -0400

    Update idris2 version to support macos

--- cBits/idris_support.c
+++ cBits/idris_support.c
@@ -4,7 +4,7 @@
 
 int idris2_isNull(void *ptr) { return (ptr == NULL); }
 void *idris2_getNull() { return NULL; }
-size_t idris2_anyptr_nat(void *ptr) { return ptr; }
+size_t idris2_anyptr_nat(void *ptr) { return (size_t) ptr; }
 
 // MEMORY OPERATIONS
 
@@ -13,7 +13,7 @@ char* idris2_heap_start() { return HEAP_START; }
 
 // utils
 
-char *UART=0x10000000;
+char *UART=(char*) 0x10000000;
 
 void putChar(char c) {
 	*UART=c;
